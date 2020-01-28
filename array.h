1a7f9b38f09d27d9acffd319c4ebf3708e0b4c86
Isaac Lin <q331074685@gmail.com>
2020-01-28 18:27:59 -0500

    changes

--- array.h
+++ array.h
@@ -17,8 +17,7 @@ class Array : public Object
         virtual size_t hash_me();
 
         // putting an item at the end of the array
-        // return 0 on success, -1 on failure
-        virtual int append(Object* oo);
+        virtual void append(Object* oo);
 
         // get the n'th item in the array.
         virtual Object* get(int nn);
@@ -27,14 +26,14 @@ class Array : public Object
         virtual int length();
 
         // set n'th element to the given object
-        // users are responsible for free'ing the pointer
-        virtual int set(int nn, Object *oo);
+        // returns the replaced Object.
+        virtual Object* set(int nn, Object *oo);
 
         // get the index of an object in the array
         virtual int index_of(Object* oo);
 
         // this compares two arrays, to see if they are the same
-        virtual bool equals(Array aa);
+        virtual bool equals(Object *oo);
 
         // print content
         // -1 on failure, 0 on success
