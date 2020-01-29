27ef2f3c8007c5f7e07da109e140f92f1d91cad4
sharonhe98 <36315984+sharonhe98@users.noreply.github.com>
2020-01-28 23:40:28 -0500

    [REMOVE] Remove implementation details
    - Remove public field "hash_" because it indicates implementation for hash()
    - Remove initialization of hash_ to 0 in constructor
    - Remove return statements in hash() and hash_me()

--- object.h
+++ object.h
@@ -5,11 +5,9 @@
 class Object
 {
     public:
-        size_t hash__;
 
         Object()
         {
-            hash__ = 0;
         }
 
         virtual ~Object()
@@ -19,12 +17,11 @@ class Object
 
         size_t hash()
         {
-            return hash__ ? hash__ : hash__ = hash_me();
         }
 
         virtual size_t hash_me()
         {
-            return 0;
+
         }
 
         virtual void print()
